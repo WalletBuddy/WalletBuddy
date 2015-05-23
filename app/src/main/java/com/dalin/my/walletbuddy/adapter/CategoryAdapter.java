f1e3a7da14fab68364c501875e83e6cc11c204c5
unknown <dlin1@cpp.edu>
2015-05-22 18:11:38 -0700

    Transferring work over

--- app/src/main/java/com/dalin/my/walletbuddy/adapter/CategoryAdapter.java
+++ app/src/main/java/com/dalin/my/walletbuddy/adapter/CategoryAdapter.java
@@ -1,5 +1,14 @@
 package com.dalin.my.walletbuddy.adapter;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
 
+import com.dalin.my.walletbuddy.data.CategoryData;
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>
+{
+    private List<CategoryData> categoryList;
 
-public class CategoryAdapter {
 }
